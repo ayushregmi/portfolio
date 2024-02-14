ef8c6db2acadf22334fdd545066f38da5db6e3f6
ayushregmi <aregmi29@gmail.com>
2024-02-14 12:32:26 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,7 @@ function App() {
         />
         <Routes>
           <Route path="/" element=<Home /> />
+          <Route path="/portfolio" element=<Home /> />
           <Route path="about" element=<About /> />
           <Route path="contact" element=<Contact /> />
           <Route path="skills" element=<Skill /> />
